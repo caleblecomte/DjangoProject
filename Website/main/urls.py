f44746223358be49aef6184aae24091b19ce5ceb
Caleb <caleblecomte21@gmail.com>
2022-09-12 15:28:28 -0400

    The Initial Commit

---
+++ Website/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views as v
+
+urlpatterns = [
+    path('login/', v.loginPage, name="login"),
+    path('logout/', v.logoutUser, name="logout"),
+    path('register/', v.registerPage, name="register"),
+
+    path("", v.home, name="home"),
+    path("room/<str:pk>/", v.room, name="room"),
+
+    path('create-room/', v.createRoom, name="create-room"),
+    path('update-room/<str:pk>', v.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>', v.deleteRoom, name="delete-room"),
+
+]
